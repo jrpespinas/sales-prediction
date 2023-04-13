84ba583517cf6ee417a70d497e3594fcd0b28617
jrpespinas <jrpespinas@gmail.com>
2023-04-13 18:20:16 +0800

    chore: remove health_check endpoint

--- app/routers/health_check.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(tags=['health check'])
-
-
-@router.get("/ping")
-def ping() -> dict[str, str]:
-    """Endpoint for checking if the API is accessible"""
-    return {"ping": "pong!"}
