f864fc673a560370a6244da11bea631511033acb
jrpespinas <jrpespinas@gmail.com>
2023-04-13 18:27:51 +0800

    feat: implement automated model_loader

---
+++ app/service/model_loader.py
@@ -0,0 +1,17 @@
+import os
+import glob
+import joblib
+
+
+class ModelLoader:
+    def __get_latest_model_path(self) -> str:
+        """Get latest model path"""
+        try:
+            return glob.glob("./model_store/sales_prediction*")[0]
+        except:
+            FileNotFoundError("model does not exist")
+
+    def get_latest_model(self) -> any:
+        """Return model"""
+        model_path = self.__get_latest_model_path()
+        return joblib.load(model_path)
