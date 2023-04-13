05f03e775dc7b7bf69b2a8edb216da6e0f4b3922
jrpespinas <jrpespinas@gmail.com>
2023-04-13 18:28:34 +0800

    feat: implement predict endpoint

---
+++ app/router/predict.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+from app.domain.store import StoreSchema
+from app.service.preprocessor import StorePreprocessor
+from app.service.model_loader import ModelLoader
+
+router = APIRouter(tags=['inference'])
+
+processor = StorePreprocessor()
+loader = ModelLoader()
+model = loader.get_latest_model()
+
+@router.post('/predict')
+async def predict(store: StoreSchema) -> dict[str, float]:
+    x = await processor.convert(store)
+    sales = model.predict(x)[0]
+    return {"sales": sales}
