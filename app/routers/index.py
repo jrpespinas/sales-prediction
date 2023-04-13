b5e8b9ac5dbbf17c24c2b482fa92c498bc3448d8
jrpespinas <jrpespinas@gmail.com>
2023-04-13 18:29:01 +0800

    chore: rename directory

--- app/routers/index.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(tags=['home'])
-
-
-@router.get("/")
-def index() -> dict[str, str]:
-    """Landing endpoint of the API"""
-    return {"message": "Hello, world!"}
