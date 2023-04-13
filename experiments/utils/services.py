56c6eb918ef589ac475894abed1022df78a3bb75
jrpespinas <jrpespinas@gmail.com>
2023-04-13 18:23:58 +0800

    refactor: change directory path for saving model

--- experiments/utils/services.py
+++ experiments/utils/services.py
@@ -5,7 +5,7 @@ from joblib import dump
 
 def get_current_model() -> str:
     """Retrieve filename of the existing model from the model store"""
-    path = r"../app/model_store/*.joblib"
+    path = r"../model_store/*.joblib"
     try:
         return glob.glob(path)[0]
     except:
@@ -15,7 +15,7 @@ def move_current_model_to_archive():
     """Move existing model to archive"""
     try:
         src_path = get_current_model()
-        dst_path = r"../app/model_store/archive"
+        dst_path = r"../model_store/archive"
         shutil.move(src_path, dst_path)
     except:
         return
@@ -23,7 +23,7 @@ def move_current_model_to_archive():
 def get_number_of_models_in_archive() -> int:
     """Count the number of models in the archive"""
     count = 0
-    dir_path = r"../app/model_store/archive"
+    dir_path = r"../model_store/archive/"
     for path in os.scandir(dir_path):
         if path.is_file():
             count += 1
@@ -34,4 +34,4 @@ def save_to_model_store(model):
     get_current_model()
     move_current_model_to_archive()
     model_num = get_number_of_models_in_archive() + 1
-    dump(model, f"../app/model_store/sales_prediction_{model_num}.joblib")
+    dump(model, f"../model_store/sales_prediction_{model_num}.joblib")
