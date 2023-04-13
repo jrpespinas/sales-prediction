c16dbece3f4d2d2c6e4796991b5f7a4d4e357c7a
jrpespinas <jrpespinas@gmail.com>
2023-04-13 20:17:16 +0800

    feat: add uvicorn workers

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Use the following for local testing, comment on deployment
-# uvicorn app.main:app --host 0.0.0.0 --port 8080
+# uvicorn app.main:app --host 0.0.0.0 --port 8080 --workers 4
 
 # Use the following for deployment, comment on local
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+uvicorn app.main:app --host 0.0.0.0 --port $PORT --workers 4
