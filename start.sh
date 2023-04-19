455a973a90efaae1bf45973214df1716bd939e5a
jrpespinas <jrpespinas@gmail.com>
2023-04-19 16:58:29 +0800

    chore: remove workers

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # uvicorn app.main:app --host 0.0.0.0 --port 8080 --workers 4
 
 # Use the following for deployment, comment on local
-uvicorn app.main:app --host 0.0.0.0 --port $PORT --workers 4
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
